3f22b3246ecdea9c1150f54712113e47e907a4ba
calvin tey <calvintts@mail.fresnostate.edu>
2019-07-16 01:07:30 -0700

    fix something

--- src/Content/Projects/Projects.js
+++ src/Content/Projects/Projects.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Project from './Project/Project'
 import './Projects.css'
+import {Element} from 'react-scroll';
 import calculator_img from "../../asset/calculator.png";
 const Projects = (props) => {
     return (
         <section className="projects_section">
+            <Element name="projects">
             <h2 id="projects" className="container_title">Projects</h2>
+            </Element>
             <Project
                 imageurl={calculator_img}
                 title="calculator"
