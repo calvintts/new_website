a46df6ea157b6c5aa7710a0b06ef8940c5803f48
calvin tey <calvintts@mail.fresnostate.edu>
2019-07-18 23:56:28 -0700

    added stuff

--- src/Header/Navbar.js
+++ src/Header/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
         <header>
             <nav>
                 <a id="profile-link" href="#about"><img alt="profile" id="profile-pic" src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-1/c10.0.40.40a/p40x40/40898157_1842919779137138_2782651672617287680_n.jpg?_nc_cat=110&_nc_oc=AQnOi5aqUzkx9tzh21bzx0hof5MqjWSAYyrTkRL5-7aWHE30LnRMnYt7i0nR21L8pGg&_nc_ht=scontent-sjc3-1.xx&oh=73e87aae55f139edca987b530bcd691c&oe=5D8BCAA4"/><span id="profile-name">Calvin</span></a>
-                <ul>
+                <ul className="nav-ul">
                     <li className="nav-li">
                         <Link activeClass="active" to="about" spy={true} smooth={true} duration={300} >About</Link>
                     </li>
@@ -19,7 +19,7 @@ const Navbar = () => {
                     </li>
                 </ul>
             </nav>
-        </header>
+</header>
     )
 }
 
