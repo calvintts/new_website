965a4750b6a94104a35bf1c522288d238a4da985
calvin tey <calvintts@mail.fresnostate.edu>
2019-07-19 01:24:42 -0700

    done with mobile

--- src/Header/Navbar.js
+++ src/Header/Navbar.js
@@ -2,25 +2,44 @@ import React from 'react';
 import {Link} from 'react-scroll';
 import "./Navbar.css";
 
-const Navbar = () => {
-    return(
-        <header>
-            <nav>
-                <a id="profile-link" href="#about"><img alt="profile" id="profile-pic" src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-1/c10.0.40.40a/p40x40/40898157_1842919779137138_2782651672617287680_n.jpg?_nc_cat=110&_nc_oc=AQnOi5aqUzkx9tzh21bzx0hof5MqjWSAYyrTkRL5-7aWHE30LnRMnYt7i0nR21L8pGg&_nc_ht=scontent-sjc3-1.xx&oh=73e87aae55f139edca987b530bcd691c&oe=5D8BCAA4"/><span id="profile-name">Calvin</span></a>
-                <ul className="nav-ul">
-                    <li className="nav-li">
-                        <Link activeClass="active" to="about" spy={true} smooth={true} duration={300} >About</Link>
-                    </li>
-                    <li className="nav-li">
-                        <Link activeClass="active" to="projects" spy={true} smooth={true} duration={300} >Projects</Link>
-                    </li>
-                    <li className="nav-li">
-                        <Link activeClass="active" to="contact" spy={true} smooth={true} duration={300} >Contact</Link>
-                    </li>
-                </ul>
-            </nav>
-</header>
-    )
+class Navbar extends React.Component{
+
+    state = {
+        showNav:false
+    }
+
+    toggleNavButton = () => {
+        this.setState({showNav:!this.state.showNav});
+    }
+
+    render() {
+        return (
+            <header>
+                <nav className="nav-bar">
+                    <a id="profile-link" href="#about"><img alt="profile" id="profile-pic"
+                                                            src="https://firebasestorage.googleapis.com/v0/b/my-portfolio-7930f.appspot.com/o/profile-pic.jpeg?alt=media&token=7907ea54-6920-4e49-9983-8dfa66a4072d"/><span
+                        id="profile-name">Calvin tey</span></a>
+                    <ul className={this.state.showNav?"mobile-nav-ul":"nav-ul"}>
+                        <li className={this.state.showNav?"mobile-nav-li":"nav-li"}>
+                            <Link activeClass="active" to="about" spy={true} smooth={true} duration={300}>About</Link>
+                        </li>
+                        <li className={this.state.showNav?"mobile-nav-li":"nav-li"}>
+                            <Link activeClass="active" to="projects" spy={true} smooth={true}
+                                  duration={300}>Projects</Link>
+                        </li>
+                        <li className={this.state.showNav?"mobile-nav-li":"nav-li"}>
+                            <Link activeClass="active" to="contact" spy={true} smooth={true}
+                                  duration={300}>Contact</Link>
+                        </li>
+                    </ul>
+                    <button onClick={this.toggleNavButton}className="nav-button">
+                        <img className="popup-button"
+                             src={this.state.showNav?"https://firebasestorage.googleapis.com/v0/b/my-portfolio-7930f.appspot.com/o/white-x.png?alt=media&token=bb85b0f6-ab0b-48d5-a7e7-144cd7bb2d56":"https://firebasestorage.googleapis.com/v0/b/my-portfolio-7930f.appspot.com/o/nav-button.png?alt=media&token=7b27cf8f-ccc3-4115-9e0a-fd4ce588f6f1"}/>
+                    </button>
+                </nav>
+            </header>
+        )
+    }
 }
 
 
---
+++ src/Header/Popup/Popup.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Popup = props => (
+    <div></div>
+)
+
+export default Popup;
